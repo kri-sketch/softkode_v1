31c1a4fe14371f34f8305bd42b888d56df3e4a12
amit45555 <amitnagpure111@gmail.com>
2025-07-10 22:45:48 +0530

    new changes

---
+++ src/components/Software/Software.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Software = () => {
+  return (
+    <div>
+      <h1>Welcome to Our Website</h1>
+      <p>Redefining Digital Experiences</p>
+    </div>
+  );
+};
+
+export default Software;

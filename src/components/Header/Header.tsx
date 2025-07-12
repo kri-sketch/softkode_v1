3f71e0b31c74e200a5e3471d9a89c9db0ce52455
amit45555 <amitnagpure111@gmail.com>
2025-07-12 16:18:02 +0530

    latest changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -16,12 +16,12 @@ const Header = () => {
 
       <nav className={`${styles.nav} ${isMenuOpen ? styles.show : ""}`}>
         <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Services</a></li>
-          <li><a href="#">Pricing</a></li>
-          <li><a href="#">Our story</a></li>
-          <li><a href="#">Case Study</a></li>
+          <li><a href="/home">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/services">Services</a></li>
+          <li><a href="/pricing">Pricing</a></li>
+          <li><a href="/ourStory">Our story</a></li>
+          <li><a href="/caseStudy">Case Study</a></li>
         </ul>
       </nav>
 

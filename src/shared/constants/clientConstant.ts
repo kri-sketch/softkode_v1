2dc75435531cdac947c42db70eaee2aaf009fb11
kri-sketch <76882363+kri-sketch@users.noreply.github.com>
2025-07-24 00:22:38 +0530

    latest changes

--- src/shared/constants/clientConstant.ts
+++ src/shared/constants/clientConstant.ts
@@ -2,28 +2,28 @@
 
 import dataeazeLogo from "../../shared/image/dataeaze.png";
 import nttDataLogo from "../../shared/image/nttdata.png";
-import tataAigLogo from "../../shared/image/tata.png"; 
+import tataAigLogo from "../../shared/image/tata.png";
 import enerscriptLogo from "../../shared/image/enerscript.png";
 
 export const CLIENT_TESTIMONIAL = [
   {
-  message:
-    "Softkode Technologies has been instrumental in delivering high-quality software solutions for our business. Their skilled developers bring innovative ideas to life, ensuring our projects are completed on time and to the highest standard.",
-  name: "Ajit Ratnaparkhi",
-  role: "Client",
-},
-
- {
-  message: "Amit Nagpure",
-    name: "Client Name",
-    role: "Client Role"
- },
-
- {
-  message: "Krishna Sharma",
-    name: "Client Name",
-    role: "Client Role"
- },
+    message:
+      "Softkode Technologies has been instrumental in delivering high-quality software solutions for our business. Their skilled developers bring innovative ideas to life, ensuring our projects are completed on time and to the highest standard.",
+    name: "Ajit Ratnaparkhi",
+    role: "Customer",
+  },
+  {
+    message:
+      "We are extremely satisfied with the exceptional software development services provided by Softkode Technologies. Their team of developers is highly skilled, responsive, and committed to delivering results that align perfectly with our business goals.",
+    name: "Sushil Shinde",
+    role: "Customer",
+  },
+  {
+    message:
+      "We highly value the collaboration with Softkode Technologies their developers are not only proficient but also bring a creative approach to solving complex software challenges.",
+    name: "Sushant Shinde",
+    role: "Customer",
+  },
 ];
 
 export const CLIENT_LOGOS = [
@@ -36,16 +36,8 @@ export const CLIENT_LOGOS = [
     src: nttDataLogo,
   },
   {
-    alt: "NTT Data",
-    src: nttDataLogo,
-  },
-  {
     alt: "TATA AIG",
     src: tataAigLogo,
-  },
-  {
-    alt: "Enerscript",
-    src: enerscriptLogo,
   },
   {
     alt: "Enerscript",

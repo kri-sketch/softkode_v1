cde7315eeef1d16fac5c9f972a08bc990fa5478c
kri-sketch <76882363+kri-sketch@users.noreply.github.com>
2025-12-30 17:30:00 +0530

    seo friendly

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet-async";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import OurStory from "./components/OurStory/OurStory";
@@ -12,6 +13,20 @@ import BackButton from "./shared/BackButton/BackButton";
 const App: React.FC = () => {
   return (
     <>
+      <Helmet>
+        <title>Skode | Custom Software & Digital Products</title>
+        <meta
+          name="description"
+          content="Skode builds custom software and digital products that help businesses grow. Located in Pune, India."
+        />
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://softkode.io/" />
+        <meta property="og:site_name" content="Skode | Softkode Technologies" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://softkode.io/" />
+        <meta property="og:image" content="/logo192.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Helmet>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
-import reportWebVitals  from "./reportWebVitals.js";
+import reportWebVitals from "./reportWebVitals.js";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <HelmetProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </HelmetProvider>
 );
 
 reportWebVitals();
